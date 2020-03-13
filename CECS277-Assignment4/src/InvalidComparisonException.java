d6ef9cb522cc63e11a4c6b39ce26cf68d95ed5c4
Charyl Enriquez <charyl.enriquez@student.csulb.edu>
2020-03-13 16:50:08 -0700

    added changes to exceptions

--- CECS277-Assignment4/src/InvalidComparisonException.java
+++ CECS277-Assignment4/src/InvalidComparisonException.java
@@ -1,11 +1,11 @@
 
 public class InvalidComparisonException extends RuntimeException {
-	public InvalidComparisonException() {
+	/*public InvalidComparisonException() {
 		super();
 	}
-	
+	*/
 	public InvalidComparisonException(String message1, String message2) {
 		//super();
-		System.out.print("Can't Compare class " + message1 + " with " + message2);
+		System.out.println("Can't Compare class " + message1 + " with " + message2);
 	}
 }
--- CECS277-Assignment4/src/InvalidDistanceComputationException.java
+++ CECS277-Assignment4/src/InvalidDistanceComputationException.java
@@ -5,6 +5,6 @@ public class InvalidDistanceComputationException extends RuntimeException {
 	}
 	public InvalidDistanceComputationException(String message1, String message2) {
 		//super();
-		System.out.println("can't Compute Distance Between class" + message1 + "and class " + message2 );
+		System.out.println("can't Compute Distance Between class " + message1 + " and " + message2 );
 	}
 }
