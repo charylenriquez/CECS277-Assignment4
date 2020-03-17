f8bdaf3a3b4ac55cfb01a5098f5f7f0c395e9971
Dmitry-057 <merezhko.dmitry@gmail.com>
2020-03-16 20:08:58 -0700

    Update Square.java

--- CECS277-Assignment4/src/Square.java
+++ CECS277-Assignment4/src/Square.java
@@ -1,7 +1,62 @@
 
 public class Square{
 	private Rectangle mSquare;
-	public Square() {
-		mSquare = new Rectangle("NoNameSquare",3,3);
-	}	
+
+	public Square() 
+	{
+		this("NoNameSquare");
+	}
+	
+	public Square(String name)
+	{
+		this(name,3);
+	}
+	
+	public Square(String name, int side)
+	{
+		mSquare = new Rectangle(name,side,side);
+		
+	}
+	
+	public double getArea()
+	{
+		return mSquare.getArea();
+	}
+	
+	public double getPerimeter()
+	{
+		return mSquare.getPerimeter();
+	}
+	
+	public double getDistance(Shape other)
+	{
+		return mSquare.getDistance(other);
+	}
+	
+	public String toString()
+	{
+		return "Square: " + super.toString() + ", Corner: " + mSquare.getCorner() + ", Length: " + mSquare.getLength() + ", Width: " + mSquare.getWidth();
+	}
+	
+	public boolean equals(Object other)
+	{
+		return mSquare.equals(other);
+	}
+	
+	public int compareTo(Object arg0)
+	{
+		return mSquare.compareTo(arg0);
+	}
+	
+	public void scale(int scaleFactor)
+	{
+		mSquare.scale(scaleFactor);
+	}
+	
+	public Point getCorner()
+	{
+		return mSquare.getCorner();
+	}
+	
+	
 }
